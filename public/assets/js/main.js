7133e341af52aabc99d716aa7f069e365f294e28
lot-uni <hikaru.unity.1110@gmail.com>
2022-03-11 18:46:20 +0900

    url fix and fix

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20,7 +20,7 @@ function send(){
         let element = document.getElementById("content_result");
         let result = document.getElementById("url_value");
         element.classList.remove("clear");
-        result.innerHTML = "http://localhost:55015/"+obj.url_short;
+        result.innerHTML = "https://l.combu.dev/"+obj.url_short;
         var tableElem = document.getElementById('table');
         var trElem = tableElem.tBodies[0].insertRow(-1);
         trElem.insertCell(0).appendChild(document.createTextNode(obj.url_short));
