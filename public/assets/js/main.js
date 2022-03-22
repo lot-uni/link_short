0b2d5df3eeb366dadf8a4f211be3b2cd102c9d44
lot-uni <hikaru.unity.1110@gmail.com>
2022-03-22 15:08:54 +0900

    qr code plus

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,7 +8,8 @@ function copy_to_clipboard() {
 }
 function send(){
     let value = document.getElementById("url").value;
-
+    let qr_img = document.getElementById("qr_img");
+    qr_img.classList.add("clear");
     $.ajax({
         url: location.protocol+"//"+location.host+"/"+"send",
         type: "GET",
@@ -19,8 +20,11 @@ function send(){
         let obj = JSON.parse(data);
         let element = document.getElementById("content_result");
         let result = document.getElementById("url_value");
+        let qr_img = document.getElementById("qr_img");
         element.classList.remove("clear");
+        qr_img.classList.remove("clear");
         result.innerHTML = location.protocol+"//"+location.host+"/"+obj.url_short;
+        qr_img.src = "https://chart.apis.google.com/chart?cht=qr&chs=150x150&chl="+document.getElementById("url_value").innerHTML;
     }).fail(function(){
         alert("通信に失敗しました。");
     });
